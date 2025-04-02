04ca64bb84dbc0b6e97bcfee085bd8cfc1098789
Josh Wolfe <joshuafwolfe@gmail.com>
2025-04-02 19:32:23 -0400

    Cleanup registers

--- src/registers.rs
+++ src/registers.rs
@@ -1,26 +1,11 @@
-macro_rules! high {
-    ($expression:expr) => {
-        ($expression >> 8) as u8
-    };
-}
-
-macro_rules! is_bit_set {
-    ($expression:expr, $number:expr) => {
-        ($expression & calc_nth_bit_power($number)) != 0
-    };
-}
-
-const fn calc_nth_bit_power(bit: u32) -> u16 {
-    2u16.pow(bit - 1)
-}
-
+// https://gbdev.io/pandocs/CPU_Registers_and_Flags.html
 #[derive(Debug, Clone, Default)]
 pub struct Registers {
     // F register contains flags
-    af: u16,
-    bc: u16,
-    de: u16,
-    hl: u16,
+    pub af: u16,
+    pub bc: u16,
+    pub de: u16,
+    pub hl: u16,
     // Stack pointer
     pub sp: u16,
     // Program counter
@@ -29,28 +14,27 @@ pub struct Registers {
 
 impl Registers {
     pub fn a(&self) -> u8 {
-        high!(self.af)
+        high_u8(self.af)
     }
 
     pub fn z_flg(&self) -> bool {
-        dbg!(calc_nth_bit_power(7));
-        is_bit_set!(self.af, 7)
+        is_bit_set(self.af, 7)
     }
 
     pub fn n_flg(&self) -> bool {
-        is_bit_set!(self.af, 6)
+        is_bit_set(self.af, 6)
     }
 
     pub fn h_flg(&self) -> bool {
-        is_bit_set!(self.af, 5)
+        is_bit_set(self.af, 5)
     }
 
     pub fn c_flg(&self) -> bool {
-        is_bit_set!(self.af, 4)
+        is_bit_set(self.af, 4)
     }
 
     pub fn b(&self) -> u8 {
-        high!(self.bc)
+        high_u8(self.bc)
     }
 
     pub fn c(&self) -> u8 {
@@ -58,7 +42,7 @@ impl Registers {
     }
 
     pub fn d(&self) -> u8 {
-        high!(self.de)
+        high_u8(self.de)
     }
 
     pub fn e(&self) -> u8 {
@@ -66,13 +50,28 @@ impl Registers {
     }
 
     pub fn h(&self) -> u8 {
-        high!(self.hl)
+        high_u8(self.hl)
     }
 
     pub fn l(&self) -> u8 {
         self.hl as u8
     }
 }
+
+const fn calc_nth_bit_power(bit: u32) -> u16 {
+    2u16.pow(bit - 1)
+}
+
+#[inline]
+fn is_bit_set(number: u16, bit: u32) -> bool {
+    (number & calc_nth_bit_power(bit)) != 0
+}
+
+#[inline]
+fn high_u8(number: u16) -> u8 {
+    (number >> 8) as u8
+}
+
 
 // TODO: Test more thouroughly
 #[cfg(test)]
@@ -85,12 +84,23 @@ mod tests {
         //        5|      10
         //
         let r = Registers {
+            af: 1290,
             bc: 1290,
+            de: 1290,
+            hl: 1290,
             ..Default::default()
         };
 
+        assert_eq!(r.a(), 5);
+
         assert_eq!(r.b(), 5);
         assert_eq!(r.c(), 10);
+
+        assert_eq!(r.d(), 5);
+        assert_eq!(r.e(), 10);
+
+        assert_eq!(r.h(), 5);
+        assert_eq!(r.l(), 10);
     }
 
     #[test]
