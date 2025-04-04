82529330055abfee7bcd5b3541a034271b2ffa9c
Josh Wolfe <joshuafwolfe@gmail.com>
2025-04-04 02:02:31 -0400

    Implement one more ld instruction

--- src/cpu.rs
+++ src/cpu.rs
@@ -20,13 +20,11 @@ impl Cpu {
         while let Some(instruction) = self.next() {
             // Match instructions that don't have any "variables"
             match instruction {
-                NOOP => {
-                    ()
-                }
+                NOOP => (),
                 HALT => {
                     return Ok(());
                 }
-                _ => {},
+                _ => {}
             };
 
             let segments = InstructionSegments::from_instruction(instruction);
@@ -60,6 +58,22 @@ impl Cpu {
                 } => {
                     let addr = self.registers.get_r16(p.try_into()?);
                     self.memory.set_byte(addr, self.registers.a());
+                }
+
+                // ld a, [r16mem]
+                InstructionSegments {
+                    x: 0,
+                    q: true,
+                    z: 2,
+                    p,
+                    ..
+                } => {
+                    let addr = self.registers.get_r16(p.try_into()?);
+
+                    let Some(data) = self.memory.get_byte(addr) else {
+                        anyhow::bail!("Out of bounds memory access at {:x}", addr);
+                    };
+                    self.registers.set_a(data);
                 }
 
                 _ => todo!(
@@ -217,7 +231,6 @@ mod tests {
         for i in 0..4 {
             let instruction = u8::from_str_radix(&format!("00{:02b}0010", i), 2)
                 .expect("Unable to parse generated number");
-            println!("{:b}", instruction);
 
             let mut cpu = Cpu::new();
             cpu.memory.memory[..2].copy_from_slice(&[
@@ -230,8 +243,32 @@ mod tests {
             cpu.process_instructions()
                 .expect("Unable to process CPU instructions");
 
-            let mem_val = cpu.memory.memory[usize::from(addr/8)];
+            let mem_val = cpu.memory.memory[usize::from(addr / 8)];
             assert_eq!(mem_val, 0b10110101);
+        }
+    }
+
+    #[test]
+    fn ld_a_r16mem() {
+        // ld a, [r16mem]
+        for i in 0..4 {
+            let instruction = u8::from_str_radix(&format!("00{:02b}1010", i), 2)
+                .expect("Unable to parse generated number");
+
+            let mut cpu = Cpu::new();
+            cpu.memory.memory[..2].copy_from_slice(&[
+                instruction,
+                0b01110110, // Halt
+            ]);
+
+            let addr = 0xDC17; // 0xC000 - 0xDFFF working mem
+            cpu.registers.set_r16(i.try_into().unwrap(), addr);
+            cpu.memory.memory[usize::from(addr / 8)] = 0x6F;
+
+            cpu.process_instructions()
+                .expect("Unable to process CPU instructions");
+
+            assert_eq!(cpu.registers.a(), 0x6F);
         }
     }
 }
