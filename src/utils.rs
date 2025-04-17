7a21330981284bdda52174f5f2f146d34971a278
Josh Wolfe <joshuafwolfe@gmail.com>
2025-04-16 22:53:55 -0400

    Implement rla & rra

--- src/utils.rs
+++ src/utils.rs
@@ -2,8 +2,9 @@
 // "it usually isn’t necessary to apply #[inline] to private functions — within a crate, the
 // compiler generally makes good inline decisions."
 
+// TODO: Genericise these
 pub(crate) const fn calc_nth_bit_power(bit: u32) -> u16 {
-    2u16.pow(bit)
+    1 << bit
 }
 
 pub(crate) const fn is_bit_set_u8(number: u8, bit: u32) -> bool {
@@ -30,11 +31,35 @@ pub(crate) const fn half_carry_sub_u16(a: u16, b: u16) -> bool {
     a & 0x0F < b & 0x0F
 }
 
+pub trait SetBit {
+    fn set_bit(&mut self, bit: u32, value: bool);
+}
+
+// TODO: Don't duplicate these
+impl SetBit for u8 {
+    fn set_bit(&mut self, bit: u32, value: bool) {
+        if value {
+            *self |= calc_nth_bit_power(bit) as u8;
+        } else {
+            *self &= !calc_nth_bit_power(bit) as u8;
+        }
+    }
+}
+
+impl SetBit for u16 {
+    fn set_bit(&mut self, bit: u32, value: bool) {
+        if value {
+            *self |= calc_nth_bit_power(bit);
+        } else {
+            *self &= !calc_nth_bit_power(bit);
+        }
+    }
+}
+
 pub trait RegisterU16Ext {
     fn set_high(&mut self, value: u8);
     fn set_low(&mut self, value: u8);
     fn high_u8(&self) -> u8;
-    fn set_bit(&mut self, bit: u32, value: bool);
 }
 
 impl RegisterU16Ext for u16 {
@@ -52,19 +77,20 @@ impl RegisterU16Ext for u16 {
         let [high, _] = self.to_be_bytes();
         high
     }
-
-    fn set_bit(&mut self, bit: u32, value: bool) {
-        if value {
-            *self |= calc_nth_bit_power(bit);
-        } else {
-            *self &= !calc_nth_bit_power(bit);
-        }
-    }
 }
 
 #[cfg(test)]
 mod test {
-    use crate::utils::RegisterU16Ext;
+    use crate::utils::{calc_nth_bit_power, RegisterU16Ext};
+
+    use super::SetBit;
+
+    #[test]
+    fn test_calc_nth_bit_power() {
+        for i in 0..16 {
+            assert_eq!(2u16.pow(i), calc_nth_bit_power(i));
+        }
+    }
 
     #[test]
     fn test_set_high() {
@@ -88,5 +114,19 @@ mod test {
 
         assert_eq!(low, new_val);
         assert_eq!(high, 0b10110001);
+    }
+
+    #[test]
+    fn test_set_bit() {
+        let mut target: u16 = 0;
+
+        target.set_bit(0, true);
+        assert_eq!(target, 0b00000000_00000001);
+        target.set_bit(8, true);
+        assert_eq!(target, 0b00000001_00000001);
+
+        let mut target: u8 = 0;
+        target.set_bit(0, true);
+        assert_eq!(target, 0b00000001);
     }
 }
